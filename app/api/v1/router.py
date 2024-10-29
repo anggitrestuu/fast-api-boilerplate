203985a75e881ac6ad0cc58223e4425b005fb2db
anggitrestuu <123190026@student.upnyk.ac.id>
2024-10-29 13:29:35 +0700

    add example endpoint

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-# from app.api.v1.endpoints import route1, route2
+from app.api.v1.endpoints import example
 
 api_router = APIRouter()
 
 # Include your endpoint routers here
+api_router.include_router(example.router, prefix="/example", tags=["example"])
 # api_router.include_router(route1.router, prefix="/route1", tags=["route1"])
 # api_router.include_router(route2.router, prefix="/route2", tags=["route2"])
